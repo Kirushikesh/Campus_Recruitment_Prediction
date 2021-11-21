1536c5158e07cba7de1d6e678941115e3ef32720
Kirushikesh DB <49152921+Kirushikesh@users.noreply.github.com>
2021-11-21 10:21:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request,flash, jsonify
 from helper import predict_output,predict_proba,CombinedAttributesAdder
 
+import __main__
+__main__.CombinedAttributesAdder= CombinedAttributesAdder
+
 app = Flask(__name__)
 app.secret_key = '8f42a73054b1749f8f58848be5e6502c'
 
